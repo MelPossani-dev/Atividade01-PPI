c341af7349b6e5dd40d0c84eadc42196a313f881
Melissa Possani <melissapossani@gmail.com>
2024-03-19 18:54:40 -0300

    atualização

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/login', (requisicao, resposta)=>{
 app.use(autenticar, express.static(path.join(process.cwd(), 'privado')));
 
 
-app.use('/cliente' , rotaCliente);
+app.use('/clientes' , rotaCliente);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
